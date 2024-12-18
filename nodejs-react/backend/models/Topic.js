a057315a5a451a14c01660134ae0950d4a3f00da
Dovran Rejepov <dowrion27@gmail.com>
2024-12-17 16:37:24 -0800

    Delete nodejs-react directory

--- nodejs-react/backend/models/Topic.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const topicSchema = new mongoose.Schema({
-    name: { type: String, unique: true, required: true },
-    description: { type: String },
-});
-
-module.exports = mongoose.model('Topic', topicSchema);
